afba3d1ae775d280850104fb433bdf09c17891a4
Steeve-feuille-ciseaux <libertybanshee1@gmail.com>
2025-07-18 11:37:34 +0200

    v0.06.1.5

--- kudo_licence/urls.py
+++ kudo_licence/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('presence/', include('presence.urls')),
+    path('', include('presence.urls')),
 ]
